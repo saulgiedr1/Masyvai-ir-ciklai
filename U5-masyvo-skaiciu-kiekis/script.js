0f38d1e181c034dd7e4e83770fa077c478cd0ad5
saulgiedr1 <saulgiedr@gmail.com>
2022-06-16 10:51:38 +0300

    neteisingas

--- U5-masyvo-skaiciu-kiekis/script.js
+++ U5-masyvo-skaiciu-kiekis/script.js
@@ -1,21 +1,28 @@
   
  window.onload=function(){
- 
+  let skaicius=10;
+
+  let ivedamasSk=ivedamas(skaicius);
+
  
   const testinis = [45, 4, 9, 16, 25,14,77];
   const didesnis = testinis.filter(didesniUz);
 
   console.log(didesnis);
  
- }      
-
-  function didesniUz(a){ 
-       
-    return  a>10;
+ }   
+  
+    function ivedamas(b){      
+      {
+        console.log(b);
+      }
+      return b;
+    }
     
-    };
-    
-    
+    function didesniUz(a){        
+      return  a > 10 //ivedamas(b);
+      
+      } 
     
      
       
