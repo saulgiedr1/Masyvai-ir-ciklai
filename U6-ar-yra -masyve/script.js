60f0899179e9c597671acff71fb06f80575c2326
saulgiedr1 <saulgiedr@gmail.com>
2022-06-27 17:24:42 +0300

    pabaigta uzduotis, kai ieskomi du skaiciai

--- U6-ar-yra -masyve/script.js
+++ U6-ar-yra -masyve/script.js
@@ -1,25 +1,44 @@
 window.onload=function(){
 
-  let ieskomasSkaicius=11;
+  let ieskomasSkaicius=129;
+  let ieskomasSkaicius1=46;
   
 
   const testinis = [5, 11, 35, 46, 65, 60, 77, 129, 22]  ;
   let arYra=arYraMasyve(testinis, ieskomasSkaicius);
+  let arYra1=arYraMasyve(testinis, ieskomasSkaicius1);
+  
+  let arYraAbu=arYraMasyveAbu(testinis,arYra,arYra1);
+  
 
 console.log(arYra);
+console.log(arYra1);
+console.log("Yra ?"+" " +arYraAbu);
 
 }
-function arYraMasyve(a,b){
+function arYraMasyve(a,b,c){
+  let arYra = false; 
+  let arYra1 = false; 
+  
 
   for (let i = 0; i < a.length; i++){
            
-  if ( a[i] === b){
+    if ( a[i] === b) {
+      return true;
+    } 
+    if(a[i]===c)  
     return true;
-  }   
-  else{
-    return false;
   }
-  
-  }       
+  return false;
+
+}
+function arYraMasyveAbu (a,b,c){
+    let arYraAbu = false
+  for (let i=0; i< a.length; i++){
+    if(b && c) {
+      return true;
+    }
+  }
+  return false;     
 
 }
---
+++ U7-ar-yra masyve-intervalas/script.js
@@ -0,0 +1,20 @@
+window.onload=function(){
+
+    let pirmasSkaicius=46;
+    let antrasSkaicius= 77;
+    
+    
+  
+    const testinis = [5, 11, 35, 46, 65, 60, 77, 129, 22]  ;
+    let arYra=arYraMasyvoIntervale(testinis, pirmasSkaicius, antrasSkaicius);
+   
+  
+  console.log(arYra);
+  
+  
+  }
+  function arYraMasyvoIntervale(a,b,c){
+  
+  
+
+  }
